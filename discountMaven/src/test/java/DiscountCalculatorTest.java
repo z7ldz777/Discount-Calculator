76e2ee82b1ae25f935d5475b36a672f11f861791
Hamzeh1111 <164568480+Hamzeh1111@users.noreply.github.com>
2025-12-21 20:08:11 +0300

    testing the discount calculator class

--- discountMaven/src/test/java/DiscountCalculatorTest.java
+++ discountMaven/src/test/java/DiscountCalculatorTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class DiscountCalculatorTest {
+        @Test
+        void newCustomerFewOrdersNoSubscription() {
+            int result =
+                    DiscountCalculator.calculateDiscount("NEW", 5, false);
+            assertEquals(5, result);
+        }
+
+        @Test
+        void newCustomerFewOrdersWithSubscription() {
+            int result = DiscountCalculator.calculateDiscount("NEW", 5, true);
+            assertEquals(7, result);
+        }
+
+        @Test
+        void regularCustomerManyOrdersWithSubscription() {
+            int result = DiscountCalculator.calculateDiscount("REGULAR", 10, true);
+            assertEquals(15, result);
+        }
+
+        @Test
+        void premiumCustomerManyOrdersWithSubscriptionCapped() {
+            int result = DiscountCalculator.calculateDiscount("PREMIUM", 10, true);
+            assertEquals(15, result);
+        }
+
+        @Test
+        void premiumCustomerFewOrdersNoSubscription() {
+            int result = DiscountCalculator.calculateDiscount("PREMIUM", 5, false);
+            assertEquals(10, result);
+        }
+
+        @Test
+        void newCustomerManyOrdersThrowsException() {
+            assertThrows(IllegalArgumentException.class, () -> {
+                DiscountCalculator.calculateDiscount("NEW", 10, true);
+            });
+
+        }
 }
